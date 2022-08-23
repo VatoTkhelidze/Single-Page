26412bf963b76407b395afcd57ad24fdbafa1741
VatoTkhelidze <vatotxelidze@gmail.com>
2022-08-23 17:20:10 +0400

    modified HTML/CSS for mobile & tablet versions and added their own functions on JS

--- task15.js
+++ task15.js
@@ -1,24 +1,81 @@
-let right_arrow = document.querySelector('.right_box');
-let left_arrow = document.querySelector('.left_box');
+let right_arrow = document.querySelector('.right_box_desktop');
+let left_arrow = document.querySelector('.left_box_desktop');
 let slideBox = document.querySelector('.slides');
 let imageFirst = document.querySelector('.slide_three');
 let CountButtonHomeClicks = 0;
 let calculateWidthGap;
 
+let right_arrow_tablet = document.querySelector('.right_box_tablet');
+let left_arrow_tablet = document.querySelector('.left_box_tablet');
+
+let right_arrow_phone = document.querySelector('.right_box_phone');
+let left_arrow_phone = document.querySelector('.left_box_phone');
 
 
 left_arrow.addEventListener('click', function sliding(){
-    calculateWidthGap = parseInt(window.getComputedStyle(imageFirst).width) + parseInt(window.getComputedStyle(slideBox).gap);
+  calculateWidthGap = parseInt(window.getComputedStyle(imageFirst).width) + parseInt(window.getComputedStyle(slideBox).gap);
+  if(calculateWidthGap * CountButtonHomeClicks < 1710 && calculateWidthGap * CountButtonHomeClicks >= -570){
     CountButtonHomeClicks += 1;
     slideBox.style.transform = `translateX(${calculateWidthGap * CountButtonHomeClicks}px)`;
     slideBox.style.transition = '1.5s';
+  }
 })
 
 right_arrow.addEventListener('click', function sliding(){
   calculateWidthGap = parseInt(window.getComputedStyle(imageFirst).width) + parseInt(window.getComputedStyle(slideBox).gap);
-  CountButtonHomeClicks -= 1;
-  slideBox.style.transform = `translateX(${calculateWidthGap * CountButtonHomeClicks}px)`;
-  slideBox.style.transition = '1.5s';
+  if(calculateWidthGap * CountButtonHomeClicks > -570 && calculateWidthGap * CountButtonHomeClicks <= 1710){
+    CountButtonHomeClicks -= 1;
+    slideBox.style.transform = `translateX(${calculateWidthGap * CountButtonHomeClicks}px)`;
+    slideBox.style.transition = '1.5s';
+  }
 })
+
+
+
+
+
+
+left_arrow_tablet.addEventListener('click', function sliding(){
+  calculateWidthGap = parseInt(window.getComputedStyle(imageFirst).width) + parseInt(window.getComputedStyle(slideBox).gap);
+  if(calculateWidthGap * CountButtonHomeClicks < 1140 && calculateWidthGap * CountButtonHomeClicks >= -1140){
+    CountButtonHomeClicks += 1;
+    slideBox.style.transform = `translateX(${calculateWidthGap * CountButtonHomeClicks}px)`;
+    slideBox.style.transition = '1.5s';
+  }
+})
+
+right_arrow_tablet.addEventListener('click', function sliding(){
+  calculateWidthGap = parseInt(window.getComputedStyle(imageFirst).width) + parseInt(window.getComputedStyle(slideBox).gap);
+  if(calculateWidthGap * CountButtonHomeClicks > -1140 && calculateWidthGap * CountButtonHomeClicks <= 1710){
+    CountButtonHomeClicks -= 1;
+    slideBox.style.transform = `translateX(${calculateWidthGap * CountButtonHomeClicks}px)`;
+    slideBox.style.transition = '1.5s';
+  }
+})
+
+
+
+
+left_arrow_phone.addEventListener('click', function sliding(){
+  calculateWidthGap = parseInt(window.getComputedStyle(imageFirst).width) + parseInt(window.getComputedStyle(slideBox).gap);
+  if(calculateWidthGap * CountButtonHomeClicks < 570 && calculateWidthGap * CountButtonHomeClicks >= -1140){
+    CountButtonHomeClicks += 1;
+    slideBox.style.transform = `translateX(${calculateWidthGap * CountButtonHomeClicks}px)`;
+    slideBox.style.transition = '1.5s';
+  }
+})
+
+right_arrow_phone.addEventListener('click', function sliding(){
+  calculateWidthGap = parseInt(window.getComputedStyle(imageFirst).width) + parseInt(window.getComputedStyle(slideBox).gap);
+  if(calculateWidthGap * CountButtonHomeClicks > -570 && calculateWidthGap * CountButtonHomeClicks <= 1710){
+    CountButtonHomeClicks -= 1;
+    slideBox.style.transform = `translateX(${calculateWidthGap * CountButtonHomeClicks}px)`;
+    slideBox.style.transition = '1.5s';
+  }
+})
+
+
+
+
 
 
